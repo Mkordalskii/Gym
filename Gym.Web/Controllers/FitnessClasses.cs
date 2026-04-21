f40aa1e703e87de05577ae45d39454ece1a067c5
Maciej <m.kordalski@gmail.com>
2026-04-21 17:45:54 +0200

    add FitnessClasses

---
+++ Gym.Web/Controllers/FitnessClasses.cs
@@ -0,0 +1,32 @@
+﻿using Gym.Data.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gym.Web.Controllers
+{
+    public class FitnessClasses : Controller
+    {
+        private readonly GymContext _context;
+        public FitnessClasses(GymContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.PageModel = await _context.PortalPage
+                .Where(p => p.IsPublished)
+                .OrderBy(p => p.Id)
+                .ToListAsync();
+            ViewBag.Parameters = await _context.Parameter
+                .Where(p => p.IsActive)
+                .ToDictionaryAsync(p => p.Name, p => p.Value);
+
+            var classes = await _context.FitnessClass
+                .Where(c => c.IsActive)
+                .OrderBy(c => c.StartTime)
+                .ToListAsync();
+
+            return View(classes);
+        }
+    }
+}
